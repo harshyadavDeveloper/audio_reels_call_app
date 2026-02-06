62822a516cdc5f4d746a4cb3aa54bd4db5fabc8a
harshyadavDeveloper <harshyadav5939@gmail.com>
2026-02-06 11:29:19 +0530

    Replace broken code with correct code

---
+++ android/app/src/main/kotlin/com/example/audio_call_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audio_call_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/permissions/permission_service.dart
@@ -0,0 +1,6 @@
+class PermissionService {
+  Future<bool> requestMicrophonePermission() async {
+    // Will implement using permission_handler
+    return true;
+  }
+}
